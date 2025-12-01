1f168b4e246be46461f26414b4c59588ac9b648a
uzzin <01uzzin@gmail.com>
2025-12-01 16:25:04 +0900

    SEO 설정

--- src/pages/book/[id].tsx
+++ src/pages/book/[id].tsx
@@ -2,6 +2,7 @@ import { GetStaticPropsContext, InferGetStaticPropsType } from "next";
 import style from "./[id].module.css";
 import fetchOneBook from "@/lib/fetch-one-book";
 import { useRouter } from "next/router";
+import Head from "next/head";
 
 export const getStaticPaths = () => {
   return {
@@ -38,7 +39,18 @@ export default function Page({
 }: InferGetStaticPropsType<typeof getStaticProps>) {
   const router = useRouter();
 
-  if (router.isFallback) return "로딩중입니다."; // fallback 상태
+  if (router.isFallback) { // fallback 상태
+    return (
+      <>
+        <Head>
+          <meta property="og:image" content="/thumbnail.png" />
+          <meta property="og:title" content="한입북스" />
+          <meta property="og:description" content="한입북스에 등록된 도서들을 만나보세요." />
+        </Head>
+        <div>로딩중입니다.</div>
+      </>
+    );
+  }
   if (!book) return "문제가 발생했습니다. 다시 시도하세요.";
 
   const {
@@ -46,17 +58,25 @@ export default function Page({
   } = book;
 
   return (
-    <div className={style.container}>
-      <div
-        className={style.cover_img_container}
-        style={{backgroundImage: `url("${coverImgUrl}")`}}
-      >
-        <img src={coverImgUrl} />
+    <>
+      <Head>
+        <title>{title}</title>
+        <meta property="og:image" content={coverImgUrl} />
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
+      </Head>
+      <div className={style.container}>
+        <div
+          className={style.cover_img_container}
+          style={{backgroundImage: `url("${coverImgUrl}")`}}
+        >
+          <img src={coverImgUrl} />
+        </div>
+        <div className={style.title}>{title}</div>
+        <div className={style.subTitle}>{subTitle}</div>
+        <div className={style.author}>{author} | {publisher}</div>
+        <div className={style.description}>{description}</div>
       </div>
-      <div className={style.title}>{title}</div>
-      <div className={style.subTitle}>{subTitle}</div>
-      <div className={style.author}>{author} | {publisher}</div>
-      <div className={style.description}>{description}</div>
-    </div>
-  )
+    </>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,3 @@
-// CSS Module
 import SearchableLayout from "@/components/searchable-layout";
 import style from "./index.module.css";
 import { ReactNode } from "react";
@@ -6,11 +5,10 @@ import BookItem from "@/components/book-item";
 import { InferGetStaticPropsType } from "next";
 import fetchBooks from "@/lib/fetch-books";
 import fetchRandomBooks from "@/lib/fetch-random-books";
+import Head from "next/head";
 
 // Incremental Static Regeneration ( ISR )
 export const getStaticProps = async () => {
-  console.log("SSG");
-
   const [allBooks, recoBooks] = await Promise.all([
     fetchBooks(),
     fetchRandomBooks()
@@ -26,16 +24,24 @@ export default function Home({
 }: InferGetStaticPropsType<typeof getStaticProps>) {
 
   return (
-    <div className={style.container}>
-      <section>
-        <h3>지금 추천하는 도서</h3>
-        {recoBooks.map((book) => <BookItem key={book.id} {...book} />)}
-      </section>
-      <section>
-        <h3>등록된 모든 도서</h3>
-        {allBooks.map((book) => <BookItem key={book.id} {...book} />)}
-      </section>
-    </div>
+    <>
+      <Head>
+        <title>한입북스</title>
+        <meta property="og:image" content="/thumbnail.png" />
+        <meta property="og:title" content="한입북스" />
+        <meta property="og:description" content="한입북스에 등록된 도서들을 만나보세요." />
+      </Head>
+      <div className={style.container}>
+        <section>
+          <h3>지금 추천하는 도서</h3>
+          {recoBooks.map((book) => <BookItem key={book.id} {...book} />)}
+        </section>
+        <section>
+          <h3>등록된 모든 도서</h3>
+          {allBooks.map((book) => <BookItem key={book.id} {...book} />)}
+        </section>
+      </div>
+    </>
   );
 }
 
